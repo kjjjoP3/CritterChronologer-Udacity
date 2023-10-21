a8c53a502b811f145b70156178dbc47de3b6bf25
ThanhBT12 <thanhvlok0905@outlook.com.vn>
2023-10-21 20:36:04 +0700

    setup project

---
+++ src/test/java/com/udacity/jdnd/course3/critter/CritterChronologer/CritterChronologerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.jdnd.course3.critter.CritterChronologer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CritterChronologerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
