0351429c00001e17125a084289d507ac32804754
ThanhBT12 <thanhvlok0905@outlook.com.vn>
2023-10-23 18:54:59 +0700

    update code finail

--- src/main/java/com/udacity/jdnd/course3/critter/entities/Humanoid.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entities/Humanoid.java
@@ -1,32 +1,22 @@
 package com.udacity.jdnd.course3.critter.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
-@Entity
+@MappedSuperclass
 public class Humanoid {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "humanoid_seq")
-    @SequenceGenerator(name = "humanoid_seq", sequenceName = "humanoid_seq", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     String name;
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
